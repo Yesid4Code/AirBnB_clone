127c092bbf33e49d2aebe58b79aa31db52abe89b
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-25 20:35:43 +0000

    errors fixed

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,12 @@ class BaseModel:
     """ Define all common attributes/methods for others classes. """
     def __init__(self):
         self.id = str(uuid4())
-        self.create_at = datetime.now()
-        self.update_at = datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ String representation  """
-        print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """ Update the public instance attribute updated_at with the current datetime."""
@@ -25,8 +25,8 @@ class BaseModel:
         Returns a dictionary containing all keys/values of
         __dict__ of the instance.
         """
-        dic = self.__dict__.copy()
-        dict['create_at'] = self.create_at.isoformat()
-        dict['update_at'] = self.uptade_at.isoformat()
+        dict = self.__dict__.copy()
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
         dict['__class__'] = self.__class__.__name__
         return dict
