fd889fcf49719dc653935442ef11974218dcf101
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-26 02:08:37 +0000

    link BaseModel with FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Class BaseModel """
-
+import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -20,6 +20,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ String representation  """
@@ -29,6 +30,7 @@ class BaseModel:
     def save(self):
         """ Public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
