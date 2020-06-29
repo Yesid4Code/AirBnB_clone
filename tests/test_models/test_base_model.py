3574d274521802f3edd7762c5f667b20e382e09e
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-29 15:22:18 +0000

    Tests for Pep8 and Docs added

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,37 @@
 #!/usr/bin/python3
 """ Test BaseModel for expected behavior and documentation. """
+import unittest
+import pep8
+from datetime import datetime
+from models.base_model import BaseModel
+
+BaseModel = models.base_model.BaseModel
+
+class TestPep8B(unittest.TestCase):
+    """ check for pep8 validation """
+    def test_pep8(self):
+        """ test base and test_base for pep8 conformance """
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/base.py'
+        file2 = 'tests/test_models/test_base.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")
+
+
+class TestDocsB(unittest.TestCase):
+    """ check for documentation """
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(base_model.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ check for documentation """
+        self.assertTrue(len(BaseModel.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ check for method documentation """
+        for func in dir(BaseModel):
+            self.assertTrue(len(func.__doc__) > 0)
+class TestBaseModel():
+    """ . """
