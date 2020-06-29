fd9fd00bf072755b4287c5bc4b6117478eed0504
Andmontc <oscarandresmontes@gmail.com>
2020-06-29 23:20:52 +0000

    Tests for class FileStorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Test Filestorage containing classes to test on the FileStorage class:
+    * Functionality.
+    * Style.
+    * Documentation.
+"""
+import unittest
+import pep8
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.engine import file_storage
+
+
+class TestPep8B(unittest.TestCase):
+    """ Check for pep8 validation. """
+    def test_pep8(self):
+        """ test base and test_base for pep8 conformance """
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/engine/file_storage.py'
+        file2 = 'tests/test_models/test_engine/test_file_storage.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")
+
+
+class TestDocsB(unittest.TestCase):
+    """ Check for documentation. """
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(file_storage.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ check for documentation """
+        self.assertTrue(len(file_storage.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ check for method documentation """
+        for func in dir(FileStorage):
+            self.assertTrue(len(func.__doc__) > 0)
+
+if __name__ == '__main__':
+    unittest.main()
