ee712b90caa59cf048923bbf96340ba1fa79d83c
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-29 16:02:48 +0000

    Path of files changed

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-""" Test BaseModel for expected behavior and documentation. """
+"""
+Test BaseModel  containing classes to test on the BassModel class:
+    * Functionality.
+    * Style.
+    * Documentation.
+"""
 import unittest
 import pep8
 from datetime import datetime
+from models import base_model
 from models.base_model import BaseModel
 
-BaseModel = models.base_model.BaseModel
 
 class TestPep8B(unittest.TestCase):
-    """ check for pep8 validation """
+    """ Check for pep8 validation. """
     def test_pep8(self):
         """ test base and test_base for pep8 conformance """
         style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/base.py'
-        file2 = 'tests/test_models/test_base.py'
+        file1 = 'models/base_models.py'
+        file2 = 'tests/test_models/test_base_model.py'
         result = style.check_files([file1, file2])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warning).")
 
 
 class TestDocsB(unittest.TestCase):
-    """ check for documentation """
+    """ Check for documentation. """
     def test_module_doc(self):
         """ check for module documentation """
         self.assertTrue(len(base_model.__doc__) > 0)
@@ -33,5 +38,15 @@ class TestDocsB(unittest.TestCase):
         """ check for method documentation """
         for func in dir(BaseModel):
             self.assertTrue(len(func.__doc__) > 0)
+
+
 class TestBaseModel():
-    """ . """
+    """
+    Class containing methods to test on BaseModel class:
+    """
+    pass
+
+
+# Test
+# python3 -m unittest discover tests
+# python3 -m unittest tests/test_models/test_base.py
