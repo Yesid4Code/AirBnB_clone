869ef22683a2c6c37ed2a4870e7e39d058193c87
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-29 21:26:48 +0000

    Test to_dict method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ Test BaseModel  containing classes to test on the BassModel class:
 """
 import unittest
 import pep8
+import time
 from datetime import datetime
 from models import base_model
 from models.base_model import BaseModel
@@ -40,27 +41,47 @@ class TestDocsB(unittest.TestCase):
             self.assertTrue(len(func.__doc__) > 0)
 
 
-class TestBaseModel():
+class TestBaseModel(unittest.TestCase):
     """ Check for functionality. """
-    def SetUp(self):
+    def setUp(self):
         """ Method to set the star point. """
         self.model0 = BaseModel()
         self.model1 = BaseModel()
 
+    def tearDown(self):
+        """ . """
+        del self.model0
+        del self.model1
+
     def test_is_instance(self):
         """ Check if a variable is an instance. """
-        self.assertIsIstance(self.model0, BaseModel)
+        self.assertIsInstance(self.model0, BaseModel)
 
     def test_id(self):
         """ Check if id of a instance is a string or equal to another one. """
-        self.asserNotEqual(self.model0.id, self.model1.id)
-        self.asserEqual(type(self.model0.id), str)
+        self.assertNotEqual(self.model0.id, self.model1.id)
+        self.assertEqual(type(self.model0.id), str)
 
     def test_save(self):
         """ Test if the method 'save' is working. """
-        old_create_at = self.model0.create_at
+        created_at = self.model0.created_at
         self.model0.save()
-        
+        updated_at = self.model0.updated_at
+        # dic0 = model0.to_dict()
+        self.assertNotEqual(created_at, updated_at)
+        # self.assertNotEqual(dict0['created_at'], dict0['updated_at'])
+
+    def test_to_dict(self):
+        """ Test if the method return a dictionary correctly. """
+        form = "%Y-%m-%dT%H:%M:%S.%f"
+        dic = self.model0.to_dict()
+        self.assertEqual(dic["__class__"], "BaseModel")
+        self.assertEqual(type(dic["created_at"]), str)
+        self.assertEqual(dic["created_at"],
+                         self.model0.created_at.strftime(form))
+        self.assertEqual(dic["updated_at"],
+                         self.model0.updated_at.strftime(form))
+
 
 # Test
 # python3 -m unittest discover tests
