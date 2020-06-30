bdf8edb0c9bbcbc6bdcc5322431b7fdcddcef073
Andmontc <oscarandresmontes@gmail.com>
2020-06-30 20:50:57 +0000

    change in reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,15 +10,17 @@ from models.state import State
 from models.base_model import BaseModel
 
 
+# classes
+classes = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity,
+           "Review": Review, "City": City, "Place": Place, "State": State}
+
+
 class FileStorage:
     """
     Class that:
         - serializes instances to a JSON file.
         - deserializes JSON file to instances.
     """
-    classes = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity,
-               "Review": Review, "City": City, "Place": Place, "State": State}
-
     __file_path = "file.json"
     __objects = {}
 
@@ -50,6 +52,7 @@ class FileStorage:
             with open(FileStorage.__file_path, "r") as fil:
                 listt = json.load(fil)
             for key, val in listt.items():
-                self.__objects[key] = self.classes[val["__class__"]](**val)
+                # key_class = key.split(".")
+                FileStorage.__objects[key] = classes[val["__class__"]](**val)
         except:
             pass
